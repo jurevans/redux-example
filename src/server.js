f266eadf874dd2b12bcce21b3f48c4631bd55810
Justin Evans <jurevans@gmail.com>
2016-08-11 11:00:30 -0400

    Add event dispatching, data

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
 import Server from 'socket.io';
 
-export default function startServer() {
-	const io = new Server().attach(8090);
+export function startServer(store) {
+	let port = 8090;
+	const io = new Server().attach(port);
+
+	store.subscribe(
+		() => io.emit('state', store.getState().toJS())
+	);
+
+	io.on('connection', (socket) => {
+		socket.emit('state', store.getState().toJS());
+		socket.on('action', store.dispatch.bind(store));
+	});
+
+	console.log('Server started at http://localhost:' + port + '/');
 }
